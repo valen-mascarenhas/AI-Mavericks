0c51961394c8728708231a314df8e213b15a400f
Sachin Itagi <sachin.itagi@ibm.com>
2025-03-22 12:59:26 +0000

    Add Multiple Graphs

--- src/components/ecommerce/MonthlySalesChart.tsx
+++ src/components/ecommerce/MonthlySalesChart.tsx
@@ -13,7 +13,7 @@ const ReactApexChart = dynamic(() => import("react-apexcharts"), {
 
 export default function MonthlySalesChart() {
   const options: ApexOptions = {
-    colors: ["#465fff"],
+    colors: ["#465fff", "#4bb738", "#d37d17"],
     chart: {
       fontFamily: "Outfit, sans-serif",
       type: "bar",
@@ -25,8 +25,8 @@ export default function MonthlySalesChart() {
     plotOptions: {
       bar: {
         horizontal: false,
-        columnWidth: "39%",
-        borderRadius: 5,
+        columnWidth: "80%",
+        borderRadius: 1,
         borderRadiusApplication: "end",
       },
     },
@@ -40,18 +40,11 @@ export default function MonthlySalesChart() {
     },
     xaxis: {
       categories: [
-        "Jan",
-        "Feb",
-        "Mar",
-        "Apr",
-        "May",
-        "Jun",
-        "Jul",
-        "Aug",
-        "Sep",
-        "Oct",
-        "Nov",
-        "Dec",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
       ],
       axisBorder: {
         show: false,
@@ -83,19 +76,35 @@ export default function MonthlySalesChart() {
     },
 
     tooltip: {
-      x: {
-        show: false,
-      },
-      y: {
-        formatter: (val: number) => `${val}`,
-      },
+      custom: function({ series, seriesIndex, dataPointIndex, w }) {
+        const menus = [
+          "Beans Neutrella, Aloo Mutter, Plain Rice, Dal Tadka",
+          "Zunka, Matki Usal, Plain Rice, Varan",
+          "Padwal Sukha, Paneer Kolhapuri, Shahjahani Pulav, Dal Fry, Madhur Milan",
+          "Remark: Holiday, No menu",
+          "Cabbage Foogat, Veg Kofta, Curd Rice, Sambar, Remark: Holiday Attached with Weekend"
+        ];
+        const menuItems = menus[dataPointIndex].split(', ').join('<br/>');
+        return '<div class="arrow_box" style="max-width: 100%; word-wrap: break-word;">' +
+          '<span>Food menu:<br/>' + menuItems + '</span>' +
+          '</div>';
+      }
     },
   };
   const series = [
     {
-      name: "Sales",
-      data: [168, 385, 201, 298, 187, 195, 291, 110, 215, 390, 280, 112],
+      name: "Footfall Count",
+      data: [1062, 1136, 1178, 0, 600],
     },
+    {
+      name: "Predicted food Count",
+      data: [850, 930, 1100, 0, 542],
+    },
+    {
+      name: "Actual food Count",
+      data: [860, 950, 1130, 0, 570],
+    },
+
   ];
   const [isOpen, setIsOpen] = useState(false);
 
@@ -111,44 +120,21 @@ export default function MonthlySalesChart() {
     <div className="overflow-hidden rounded-2xl border border-gray-200 bg-white px-5 pt-5 dark:border-gray-800 dark:bg-white/[0.03] sm:px-6 sm:pt-6">
       <div className="flex items-center justify-between">
         <h3 className="text-lg font-semibold text-gray-800 dark:text-white/90">
-          Monthly Sales
+          Weekly Sales
         </h3>
-
-        <div className="relative inline-block">
-          <button onClick={toggleDropdown} className="dropdown-toggle">
-            <MoreDotIcon className="text-gray-400 hover:text-gray-700 dark:hover:text-gray-300" />
-          </button>
-          <Dropdown
-            isOpen={isOpen}
-            onClose={closeDropdown}
-            className="w-40 p-2"
-          >
-            <DropdownItem
-              onItemClick={closeDropdown}
-              className="flex w-full font-normal text-left text-gray-500 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-white/5 dark:hover:text-gray-300"
-            >
-              View More
-            </DropdownItem>
-            <DropdownItem
-              onItemClick={closeDropdown}
-              className="flex w-full font-normal text-left text-gray-500 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-white/5 dark:hover:text-gray-300"
-            >
-              Delete
-            </DropdownItem>
-          </Dropdown>
-        </div>
       </div>
 
       <div className="max-w-full overflow-x-auto custom-scrollbar">
-        <div className="-ml-5 min-w-[650px] xl:min-w-full pl-2">
+        <div className="-ml-5 min-w-[600px] xl:min-w-full pl-2">
           <ReactApexChart
             options={options}
             series={series}
             type="bar"
-            height={180}
+            height={300}
           />
         </div>
       </div>
     </div>
   );
 }
+ 
